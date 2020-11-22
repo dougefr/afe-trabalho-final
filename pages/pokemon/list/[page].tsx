ebae20e5698541c0e5b8ad1b5ad37fb841f4e73a
Douglas Rodrigues <1039315@sga.pucminas.br>
2020-11-22 17:02:07 -0300

    fix vuild

--- pages/pokemon/list/[page].tsx
+++ pages/pokemon/list/[page].tsx
@@ -105,7 +105,7 @@ export default function List({ results, params }) {
   }
 
   function handleClickDetail(id: number) {
-    router.push(`/pokemon/detail/${id}`);
+    Router.push(`/pokemon/detail/${id}`);
   }
 }
 
